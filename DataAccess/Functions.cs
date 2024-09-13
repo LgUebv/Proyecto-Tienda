121090056b11662f662cdc2fe2bb5195c2165ba5
Jose Vega <lguebv@gmail.com>
2024-09-12 18:15:41 -0600

    Se creo la base del programa

---
+++ DataAccess/Functions.cs
@@ -0,0 +1,39 @@
+﻿using ConexionChacarron;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Functions
+    {
+        Base b = new Base("localhost", "root", "", "tienda");
+
+        public string Guardar(string q)
+        {
+            return b.comandos(q);
+        }
+
+        public DataSet Mostrar(string q, string tabla)
+        {
+            return b.Mostrar(q, tabla);
+        }
+
+        public string Borrar(string q)
+        {
+            return b.comandos(q);
+        }
+
+        public string Modificar(string q)
+        {
+            return b.comandos(q);
+        }
+        public string ObtenerDato(string q, string tabla, string campo)
+        {
+            return b.ObtenerDatos(q, tabla, campo);
+        }
+    }
+}
